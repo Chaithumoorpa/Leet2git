4d3ee059a7f6f765dccb6a9172294bd5e86b1d60
Chaithanya Moorpa <94587865+Chaithumoorpa@users.noreply.github.com>
2026-04-07 15:52:14 -0700

    Time: 0 ms (100%), Space: 13.3 MB (48.45%) - LeetHub

---
+++ 0704-binary-search/0704-binary-search.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left, right = 0, len(nums) - 1
+    
+        while left <= right:
+            # Calculate mid using this formula to prevent potential integer overflow
+            mid = left + (right - left) // 2
+            
+            if nums[mid] == target:
+                return mid
+            elif nums[mid] < target:
+                left = mid + 1
+            else:
+                right = mid - 1
+                
+        return -1
+
+        
